9c040ce855884d1555e342e068b6be9c7a570375
Luiz Carlos de Arruda Filho <luiz.arruda@mv.com.br>
2021-07-02 20:59:50 -0300

    initial commit

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/home")
+    public String getHome(){return "home";}
+}
